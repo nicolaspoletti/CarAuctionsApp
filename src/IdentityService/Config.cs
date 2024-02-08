6060f6a2739fad74503b64d26eec813767ab0b1a
Nicolas Poletti <nicolaspoletti1@gmail.com>
2024-02-08 19:27:06 +1100

    Gateway Service finished

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -14,7 +14,7 @@ public static class Config
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("auctionApp", "Auctionapp full access")
+            new ApiScope("auctionApp", "Auction app full access")
         };
 
     public static IEnumerable<Client> Clients =>
@@ -28,6 +28,18 @@ public static class Config
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = new[]{new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "nextApp", 
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000?api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600 * 24 * 30
             }
         };
 }
---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,26 @@
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId); 
+
+        if (context.Message.Itemsold)
+        {
+            auction.Winner = context.Message.Winner;
+
+            auction.SoldAmount = (int) context.Message.Amount;
+        }
+
+        auction.Status = "Finished";
+
+        await auction.SaveAsync();
+    }
+}
